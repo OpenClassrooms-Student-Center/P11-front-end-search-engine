f98f1ae3a0bf028c83cb6ec6a588bad78c302df2
Etha <76168237+etharion29@users.noreply.github.com>
2021-10-22 18:27:50 +0200

    First basic functions

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+/* DOM */
+
+
+
+
+/* Research and development */
+
+
+
+
+
+/* Functions */
+function myFunctionDropdown(event,myDropdown){
+    let element = event.target;
+    while(element.nodeName !== "BUTTON"){
+      element = element.parentNode;
+    }
+    document.getElementById(myDropdown).classList.toggle("hidden");
+    document.getElementById(myDropdown).classList.toggle("flex");
+}
+
+function myFunctionCloseTag(event,myTag){
+  let element = event.target;
+  while(element.nodeName !== "BUTTON"){
+    element = element.parentNode;
+  }
+  document.getElementById(myTag).classList.toggle("hidden");
+}
