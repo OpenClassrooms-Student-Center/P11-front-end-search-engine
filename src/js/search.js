263c0158e15b2fa4f6f2c318430e0d05f18ba14e
Etha <76168237+etharion29@users.noreply.github.com>
2022-01-12 21:13:47 +0100

    Reinstall project two

---
+++ src/js/search.js
@@ -0,0 +1,20 @@
+const filterByName = (recipes, filtre) => 
+    recipes.filter(recipe => recipe.name.toLowerCase().includes(filtre.toLowerCase()))
+
+
+const filterByIngredient = (recipes, filtre) =>
+    recipes.filter(recipe =>
+        recipe.ingredients.find(ingredient =>
+            ingredient.ingredient.toLowerCase().includes(filtre.toLowerCase()
+            )
+        )
+    )
+
+
+const search = (recipes, search) => {
+    const resultsByName  = filterByName(recipes, search);
+    const resultsByIngre  = filterByIngredient(recipes, search);
+    return resultsByName.concat(resultsByIngre)
+}
+
+export default search;
