a65e1ca0b1a4578606bce5d5666bcdcfb11b7e07
Corentin <76168237+etharion29@users.noreply.github.com>
2022-01-27 22:42:04 +0100

    Fix caractere limited for working with searchBar

--- src/js/searchBar/searchAlgo.js
+++ src/js/searchBar/searchAlgo.js
@@ -21,6 +21,36 @@ const filterByMultipleIngredient = (recipes, filtreIngredients) => {
     })
 }
 
+const filterByAppliance = (recipes, filtre) =>
+    recipes.filter(recipe =>
+        recipe.appliance.find(appliance =>
+            appliance.toLowerCase().includes(filtre.toLowerCase()
+            )
+        )
+    )
+
+const filterByMultipleAppliance = (recipes, filtreAppliances) => {
+    return recipes.filter(recipe => {
+        const applianceRecipe = recipe.appliance.map(appliance => appliance)
+        return filtreAppliances.every(elem => applianceRecipe.includes(elem))
+    })
+}
+
+const filterByUstensil= (recipes, filtre) =>
+    recipes.filter(recipe =>
+        recipe.ustensils.find(ustensil =>
+            ustensil.ustensil.toLowerCase().includes(filtre.toLowerCase()
+            )
+        )
+    )
+
+const filterByMultipleUstensil = (recipes, filtreUstensils) => {
+    return recipes.filter(recipe => {
+        const ustensilRecipe = recipe.ustensils.map(ustensil => ustensil.ustensil)
+        return filtreUstensils.every(elem => ustensilRecipe.includes(elem))
+    })
+}
+
 const searchAlgo = () => {
     const searchValue = searchBar.value
     const activeFilter = {
@@ -28,44 +58,56 @@ const searchAlgo = () => {
         ustensil: [],
         appliance: [],
     }
-    const tags = document.querySelectorAll('.tag');
-    tags.forEach(tag => {
-        const type = tag.getAttribute('data-type')
-        const label = tag.getAttribute('data-label')
-        activeFilter[type].push(label)
-    })
+    if (searchValue.length >= 3 ) {
+        const tags = document.querySelectorAll('.tag');
+        tags.forEach(tag => {
+            const type = tag.getAttribute('data-type')
+            const label = tag.getAttribute('data-label')
+            activeFilter[type].push(label)
+        })
 
 
-    /* TODO
+            /* TODO
 
-   - Remettre la limite de 3 carac sur la searchbar
-   - Trouver pourquoi le tag lait de coco ne rends que 2 element et pas 3
-   - Rajouter tout les filtre (appliance & ustensilt) pour la searchbar ET les tags
+        - Remettre la limite de 3 carac sur la searchbar => OK maisvoir pour reset lorsque nous sommes inférieur à 2 carac
+        - Trouver pourquoi le tag lait de coco ne rends que 2 element et pas 3
+        - Rajouter tout les filtre (appliance & ustensilt) pour la searchbar ET les tags
 
-     */
+            */
+   
+        const resultsByName  = filterByName(recipesInit, searchValue);
+        const resultsByIngre  = filterByIngredient(recipesInit, searchValue);
+        const resultsByAppliance  = filterByAppliance(recipesInit, searchValue);
+        const resultsByUstensil  = filterByUstensil(recipesInit, searchValue);
+        let recipes =  getUniqueListBy(resultsByName.concat(resultsByIngre, resultsByAppliance, resultsByUstensil), 'id')
 
-    const resultsByName  = filterByName(recipesInit, searchValue);
-    const resultsByIngre  = filterByIngredient(recipesInit, searchValue);
-    let recipes =  getUniqueListBy(resultsByName.concat(resultsByIngre), 'id')
+        if (activeFilter.ingredient.length > 0) {
+            recipes = filterByMultipleIngredient(recipesInit, activeFilter.ingredient);
+        }
+        if (activeFilter.appliance.length > 0) {
+            recipes = filterByMultipleAppliance(recipesInit, activeFilter.appliance);
+        }
+        if (activeFilter.ustensil.length > 0) {
+            recipes = filterByMultipleUstensil(recipesInit, activeFilter.ustensil);
+        }
 
+        console.log(
+            "=== Active Filter ===",
+            activeFilter
+        )
 
+        console.log(
+            "=== Recipes ===",
+            recipes
+        )
 
-    if (activeFilter.ingredient.length > 0) {
-        recipes = filterByMultipleIngredient(recipesInit, activeFilter.ingredient);
+        return recipes
+
+    } else if (searchValue.length <= 2) {
+
+        return recipesInit
+        
     }
-
-    console.log(
-        "=== Active Filter ===",
-        activeFilter
-    )
-
-
-    console.log(
-        "=== Recipes ===",
-        recipes
-    )
-
-    return recipes
 
 }
 
