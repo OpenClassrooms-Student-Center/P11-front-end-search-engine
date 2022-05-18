719e3fa1761f189af4e084d0d915328d5306e79e
OSFR34 <selma-ozturk@outlook.com>
2022-05-18 13:42:21 +0200

    ajout page displayRecipe

---
+++ js/displayRecipes.js
@@ -0,0 +1,8 @@
+function displayRecipe (data) {
+
+    const { id, name, ingredients, ingredient, times, description, appliance, ustensils} = data;
+    
+    console.log (id)
+
+}
+displayRecipe(recipes)
