1002cfc45bafe8415454df0900f0408ce8690b48
OSFR34 <selma-ozturk@outlook.com>
2022-06-04 00:02:34 +0200

    début algo 2

--- js/2searchFunctionnalProgramming.js
+++ js/2searchFunctionnalProgramming.js
@@ -24,7 +24,7 @@ for (let i=0; i<recipes.length; i++) {
     });
 }
 const ingredientsNoDuplicates = new Set(allIngredients);
-console.log(ingredientsNoDuplicates);
+// console.log(ingredientsNoDuplicates);
 
 const allAppliances = [];
 for (let i=0; i<recipes.length; i++) {
@@ -32,7 +32,7 @@ for (let i=0; i<recipes.length; i++) {
     allAppliances.push(appliances);
 }
 const appliancesNoDuplicates = new Set(allAppliances);
-console.log(appliancesNoDuplicates);
+// console.log(appliancesNoDuplicates);
 
 const allUstensils = [];
 for (let i=0; i<recipes.length; i++) {
@@ -44,10 +44,40 @@ const ustensilsNoDuplicates = new Set(allUstensils.flat());
 
 
 for (const element of ustensilsNoDuplicates) {
-    console.log(element.toLowerCase());
+    // console.log(element.toLowerCase());
 }
 
-console.log(ustensilsNoDuplicates);
+// console.log(ustensilsNoDuplicates);
+
+// BARRE DE RECHERCHE ECOUTEUR D'EVENEMENT SUR PRESSION DU CLAVIER
+const searchInput = document.querySelector("#search");
+
+// la méthode .keyup() permet de mettre l'écouteur d'événement dès qu'une touche est pressée- (e) pour évèvement(ds la console il correspond à un tableau de bord avec de nombreux élément dont value qui sera les caractères saisis)
+searchInput.addEventListener("keyup",(e) => { 
+    // target est l'un des élément de l'évènement(e), ici c'est l'input
+    console.log(e)
+    const searchedLetters = e.target.value;
+    console.log(searchedLetters)
+    // récupère toutes les cartes de recettes 
+    const cardsRecipes = document.querySelectorAll(".recipe_article");
+    // console.log(cardsRecipes)
+    filterElements(searchedLetters, cardsRecipes)
+});
+
+// elements correspond aux cartes et letters aux lettres saisies
+function filterElements (letters, elements) {
+    // la fct se déclenchera uniquement lorsque l'utilisateur auras saisi plus de 2 caractèress.
+    if (letters.length >2) {
+        for (let i=0; 1<elements.length; i++){
+            // si les lettres saisies est inclut dans les cartes de recettes 
+            if(elements[i].textContent.toLowerCase().includes(letters)) {
+                elements[i].style.display = "block";        
+            }else {
+                elements[i].style.display = "none";
+            }
+        }
+    }
+}
 
 
 
