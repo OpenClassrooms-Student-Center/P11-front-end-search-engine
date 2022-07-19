a14e9b711f50895abc43a93b3731c02e69b39f1f
Khaera MB <khaera.belkadi@gmail.com>
2022-07-19 19:17:14 +0200

    refactoring Code

--- script/App.js
+++ script/App.js
@@ -1,64 +1,27 @@
 import recipes from "../data/recipes.js";
 import CardRecipesFactory from "./Factory/CardRecipesFactory.js";
+import Filter from "./Filter/Filter.js";
 
-export default class App{
-    constructor(){
+export default class App {
+    constructor() {
         this.recipes = recipes
         this.input = document.getElementById("find")
         this.tag = document.getElementById("tag")
         this.close = document.querySelector('.fa-times-circle')
     }
-    displayAllRecipes(){
-        const viewCard =  new CardRecipesFactory(this.recipes)
+    displayAllRecipes() {
+        const viewCard = new CardRecipesFactory(this.recipes)
         viewCard.AllRecipes()
-       
+
     }
-    
-    filterRecipes(){
-        this.input.addEventListener("keyup", (e)=>{
-                const searchString = e.target.value
-                const filterRecipe = this.recipes.filter(result =>{
-                    if (this.input.value === "")
-                    {
-                        this.tag.classList.add("d-none")
-                        const viewCard =  new CardRecipesFactory(this.recipes)
-                        viewCard.AllRecipes()
-                    }
-                    // show tag in DOM
-                    else
-                    {
-                        this.tag.classList.remove("d-none")
-                        document.getElementById("btnIngredient").innerText = this.input.value
-                    }  
-                   
 
-                   return (
-                    result.name.toLowerCase().includes(searchString) || 
-                    result.description.toLowerCase().includes(searchString) ||
-                    result.ingredients.find(items => {
-                        return items.ingredient.toLowerCase().includes(searchString)
-                    }) != undefined
-                    )
-                   
-                })
-                const viewCard =  new CardRecipesFactory(filterRecipe)
-                viewCard.AllRecipes() 
-            })
-
-            this.close.addEventListener('click', ()=>{
-                this.input.value = ""
-                this.tag.classList.add("d-none") 
-                document.querySelectorAll("#card").forEach( (elt)=>{ 
-                    elt.remove() 
-                    const viewCard =  new CardRecipesFactory(this.recipes)
-                    viewCard.AllRecipes() 
-                }) 
-            })
-   
+    displaySort() {
+        const sort = new Filter(this.recipes)
+        sort.filterRecipes()
     }
 }
 
 // affichage dans la View
 const app = new App()
-app.displayAllRecipes() 
-app.filterRecipes()
+app.displayAllRecipes()
+app.displaySort()
---
+++ script/Filter/Filter.js
@@ -0,0 +1,51 @@
+import recipes from "../../data/recipes.js";
+import CardRecipesFactory from "../Factory/CardRecipesFactory.js";
+
+export default class Filter {
+    constructor() {
+        this.recipes = recipes
+        this.input = document.getElementById("find")
+        this.tag = document.getElementById("tag")
+        this.close = document.querySelector('.fa-times-circle')
+    }
+
+    filterRecipes() {
+        this.input.addEventListener("keyup", (e) => {
+            const searchString = e.target.value
+            const filterRecipe = this.recipes.filter(result => {
+                if (this.input.value === "") {
+                    this.tag.classList.add("d-none")
+                    const viewCard = new CardRecipesFactory(this.recipes)
+                    viewCard.AllRecipes()
+                }
+                // show tag in DOM
+                else {
+                    this.tag.classList.remove("d-none")
+                    document.getElementById("btnIngredient").innerText = this.input.value
+                }
+
+
+                return (
+                    result.name.toLowerCase().includes(searchString) ||
+                    result.description.toLowerCase().includes(searchString) ||
+                    result.ingredients.find(items => {
+                        return items.ingredient.toLowerCase().includes(searchString)
+                    }) != undefined
+                )
+
+            })
+            const viewCard = new CardRecipesFactory(filterRecipe)
+            viewCard.AllRecipes()
+        })
+
+        this.close.addEventListener('click', () => {
+            this.input.value = ""
+            this.tag.classList.add("d-none")
+            document.querySelectorAll("#card").forEach((elt) => {
+                elt.remove()
+                const viewCard = new CardRecipesFactory(this.recipes)
+                viewCard.AllRecipes()
+            })
+        })
+    }
+}
