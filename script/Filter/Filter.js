2c680115ba4282e277a1e7dabc0f58816687a62b
Khaera MB <khaera.belkadi@gmail.com>
2022-07-19 19:35:59 +0200

    link files

--- script/Filter/Filter.js
+++ script/Filter/Filter.js
@@ -1,4 +1,4 @@
-import recipes from "../../data/recipes.js";
+import recipes from "../../data/recipes.js  ";
 import CardRecipesFactory from "../Factory/CardRecipesFactory.js";
 
 export default class Filter {
