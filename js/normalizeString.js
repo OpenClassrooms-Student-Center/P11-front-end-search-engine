01b338ae09d9a58c1c950d1242e0991bd5f449e6
OSFR34 <selma-ozturk@outlook.com>
2022-07-29 14:32:23 +0200

    commentaire

--- js/normalizeString.js
+++ js/normalizeString.js
@@ -2,7 +2,7 @@ function normalizeString(string){
 
     string = string.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     
-    // les voyelles sont remplacées par une ligature
+    // les ligatures sont remplcées par des voyelles séparées.
     string = string.replace(/œ/g, "oe").replace(/æ/g, "ae").replace(/[']/g, " ");
   
     return string;
