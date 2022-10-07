988df39597f73f894e6abd6d7baf85987a4b6270
Moonkey44 <melvin.marceaux@gmail.com>
2022-10-07 21:16:08 +0200

    msie en place de la recherche dans les titres

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,44 @@
 function init(){
     const main = document.querySelector("main");
     const section = document.createElement("section");
-    const listOfIngredients = [];
-    const listOfUstensils = [];
-    const listOfAppliances = []; 
+    const listOfRecipes = [];
     section.setAttribute("tabindex","0");
     section.setAttribute("aria-label","Contenu des recettes");
     recipes.forEach(recipe => {
         recipesModel = recipesFactory(recipe);
         const recipeArticle = recipesModel.getRecipesCardDOM();
         section.appendChild(recipeArticle);
-        setTable(listOfIngredients,listOfUstensils,listOfAppliances,recipesModel);
+        listOfRecipes.push(recipeArticle);
     });
     main.appendChild(section);
-    console.log(listOfIngredients);
-    console.log(listOfUstensils);
-    console.log(listOfAppliances);
     document.getElementById("search").addEventListener("input", function(e){
-        if(e.target.value.length >= 3){
-            
+        const search = e.target.value.toLowerCase();
+        if(search.length >= 3){
+            const searchRegex = new RegExp(search);
+            const index = [];
+            console.log(index);
+            let nbrDelete = 0;
+            // console.log(listOfRecipes);
+            // console.log(searchRegex);
+            listOfRecipes.forEach(recipe     => {
+                if(!recipe.children[0].children[0].children[0].textContent.toLowerCase().match(searchRegex)){
+                    index.push(listOfRecipes.indexOf(recipe));
+                }
+            });
+            if(index !== []){
+                //on supprime les articles correspondants
+                index.forEach(index => {
+                    listOfRecipes.splice(index-nbrDelete,1);
+                    section.removeChild(section.children[index-nbrDelete]);
+                    nbrDelete += 1;
+                });
+            }
+            else{
+                
+            }
         }
     });
-}
-
-function setTable(listOfIngredients, listOfUstensils, listOfAppliances, recipesModel){
-    recipesModel.ingredients.forEach(ingredient => {
-        listOfIngredients.push(ingredient.ingredient);
-    });
-    recipesModel.ustensils.forEach( ustensil => {
-        listOfUstensils.push(ustensil);
-    });
-    listOfAppliances.push(recipesModel.appliance);
-}
-
-function sortTable(listOfIngredients,listOfUstensils,listOfAppliances){
-    
-}
+} 
 
 init();
 
