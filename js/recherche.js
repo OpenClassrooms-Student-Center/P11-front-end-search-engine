3096d7f86510e63d54f0696711141a7780ea77a8
David <david.houesnard@gmail.com>
2023-04-13 13:59:53 +0200

    Create recherche.js

---
+++ js/recherche.js
@@ -0,0 +1,11 @@
+const rechercheInput = document.querySelector(".rechercheInput");
+
+function rechercheRecettes() {
+  si plus de 2 caractères => recherche sinon j'affiche tout.
+  si saisie = nom OU ustenciles OU description => affiche les tags des recettes ET les recettes correspondantes
+  si rien ne ressort => affiche message erreur
+}
+
+function rechercheTags() {
+  si tag choisi => affiche boxTag et met à jour la liste des recettes
+}
