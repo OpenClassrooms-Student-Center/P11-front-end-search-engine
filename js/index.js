192e4c816d676d689df70b2dc5c768aaf8a47c0f
jbbrassoud <jbbrassoud@gmail.com>
2023-04-27 04:41:10 +0200

    last

--- js/index.js
+++ js/index.js
@@ -1,112 +1,118 @@
 const resultsSection = document.querySelector(".results");
-let filterList = document.querySelectorAll("dropdownUl")
+
+
+
 const ddInputs = document.querySelectorAll("dd-input")
-const ustensilsDd = document.querySelector(".dd-ustensils")
 const recipesFiltre = recipes
+const chevronUp = document.querySelectorAll(".fa-chevron-up")
 const dropdownsEl = Array.from(document.querySelectorAll(".fa-chevron-down"));
-console.log(dropdownsEl)
+const ingredientsFilter = document.getElementById("ingredients-filter")
+
+//console.log(dropdownsEl);
+
 let tags = []
 
 
-//recolte les recettes
-/* function recipesCall() {
-    recipesFiltre.forEach(recipes => {
-        const id = recipes.id
-        const nameRecipe = recipes.name
-        const servings = recipes.servings
-        const ingredients = recipes.ingredients
-        ingredients.forEach(ingredients => {
-            ingredient = ingredients.ingredient;
-            quantity = ingredients.quantity;
-            unit = ingredients.unit;
-        })
-        const time = recipes.time
-        const description = recipes.description
-        const appliance = recipes.appliance
-        const ustensils = recipes.ustensils
-        const dataRecipes = { id, nameRecipe, servings, ingredients, time, description, appliance, ustensils };
-        //console.log(time);
-        //console.log(unit);
-        //console.log(ustensils);
-    })
-} */
-
-
-//Ecoute du click pour liste déroulante
-/* dropdownsEl.addEventListener("click", (e) => {
-    console.log("event ecouté");
-    console.log(e.srcElement.classList)
-    switch (e.srcElement.classList) {
-        case '.dd-ustentils':
-            dropdownUstensils();
-            break;
-        case '.dd-apparels':
-            dropdownApparels()
-            break;
-        case '.dd-ingredients':
-            dropdownIngredients();
-            break;
-        default:
-            console.log("pas de fonctions écoutée")
-    }
-
-}) */
-
 dropdownsEl.forEach(dd =>
     dd.addEventListener("click" , (e) => {
-        console.log(e.srcElement.id)
         dropdown(e.srcElement.id);
+        chevronUp[0].style.display = "flex";
+        dropdownsEl[0].style.display = "none"
+        ingredientsFilter.style.width ="600px"
+    }))
 
+chevronUp.forEach(dd =>
+    dd.addEventListener("click" , (e) => {
+        chevronUp[0].style.display = "none";
+        dropdownsEl[0].style.display = "flex"
     }))
 
 //fonction dropdown
 async function dropdown(nom) {
-    
+
     let eleAffiche = tags[nom]
-    console.log(eleAffiche);
+
+    let uniqueArray = [...new Set(eleAffiche)];
+    console.log(nom);
+    
+    
+
+    
     //sort les listes deroulantes
-    eleAffiche.forEach((e) => {
-        filterList.innerHTML += `<li class="${nom}_>${e}</li>`;
+    uniqueArray.forEach((e) => {
+        console.log(e.srcElement);
+        //console.log(eleAffiche);
+        
+        //console.log(nom);
+        //console.log(e);
+        let filterList = e.srcElement.id
+        console.log(filterList);
+        filterList.innerHTML += '<li class="${nom}_open">' + e + '</li>';
+        
+/*        const li = document.createElement('li')
+        li.classList.add(nom + '_open')
+        li.textContent = e
+        console.log(li);
+        console.log(filterList);
+        filterList.appendChild('li')  */
+        
         
     })
+    
+
+    console.log("dropdown effectué");
 }
 
-/* ustensilsDd.addEventListener("click", () => {
-    console.log("event écouté");
-    dropdownUstensils(recipes);
-}) */
+
 
 //montre les recettes en bloc
 async function displayRecipes(recipesDisplay) {
     //tableaux vides
+    //console.log(recipesFiltre);
     let appliances = [];
     let ustensils = []
-    let ingredients = [];
+    let ingredientsArray = [];
 
+    tags['ustensils'] = [];
+    //console.log(tags['ustensils']);
+tags['appareils'] = [];
+    
+tags['ingredients'] = []; 
 
-    //apparition de la factory des blocs
+  
     recipesDisplay.forEach((recipe) => {
+
+            //remplissage des tableaux
+            recipeArray = [recipes]
+
+            appliances = [recipe.appliance]
+            
+            ustensils = [recipe.ustensils]
+
+            recipe.ingredients.forEach(ingredient => {
+                ingredientsArray = [...[ingredient.ingredient]]
+
+            })
+
+            totalIngredients = tags['ingredients'].push(ingredientsArray);
+            totalAppareils = tags['appareils'].push(appliances);
+            totalustensils = tags['ustensils'].push(ustensils);
+
+
+
+
+            //factory d'affichages des blocs
         const recipeEngine = recipesFactory(recipe);
         const recipeDOM = recipeEngine.getRecipeDOM();
         resultsSection.appendChild(recipeDOM);
-
-    //remplissage des tableaux
-        console.log(recipe);
-        recipeArray = [...new Set(recipes)]
-        appliances = [...new Set(recipe.appliance)]
         
-        ustensils = [...[recipe.ustensils]]
-        recipe.ingredients.forEach(ingredient => {
-            ingredients = [...[ingredient.ingredient]]
-        })
-        console.log(ingredients)
         
     });
-
-    tags['ustensils'] = ustensils;
-    console.log(tags['ustensils']);
-    tags['appareils'] = appliances;
-    tags['ingredients'] = ingredients;
+    tags['ingredients'] = [...new Set(tags['ingredients'])];
+    tags['appareils'] = [...new Set(tags['appareils'])];
+    tags['ustensils'] = [...new Set(tags['ustensils'])];
+    console.log(tags);
+    
 };
 
 //Pour chaque ustensile
