b9f281293e2a562c0c3c6dc9e22d10d287293c50
jbbrassoud <jbbrassoud@gmail.com>
2023-05-15 14:00:32 +0200

    avant mentorat

--- js/index.js
+++ js/index.js
@@ -17,28 +17,28 @@ let tags = []
 
 
 dropdownsEl.forEach(dd =>
-    dd.addEventListener("click" , (e) => {
+    dd.addEventListener("click", (e) => {
         ouvreListe(e.target)
         //console.log(e.target.id);
-        
+
     }))
 
 
-    function ouvreListe(e){
-        document.getElementById("chev-up-" + e.id).style.display = "flex";
-        document.getElementById(e.id + "-filter").style.maxHeight = "450px"
-        e.style.display = "none"
-        document.getElementById(e.id + "-filter").style.width = "600px"
-        
-        document.getElementById("ul-" + e.id).style.display = "flex"
-    }
+function ouvreListe(e) {
+    document.getElementById("chev-up-" + e.id).style.display = "flex";
+    document.getElementById(e.id + "-filter").style.maxHeight = "450px"
+    e.style.display = "none"
+    document.getElementById(e.id + "-filter").style.width = "600px"
+
+    document.getElementById("ul-" + e.id).style.display = "flex"
+}
 chevronUp.forEach(dd =>
-    dd.addEventListener("click" , (e) => {
+    dd.addEventListener("click", (e) => {
         //console.log(e.target.id);
         document.getElementById(e.target.id.split('-')[2]).style.display = "flex";
         document.getElementById(e.target.id.split('-')[2] + "-filter").style.maxHeight = "75px"
         e.target.style.display = "none";
-        document.getElementById(e.target.id.split('-')[2] + "-filter").style.width ="auto"
+        document.getElementById(e.target.id.split('-')[2] + "-filter").style.width = "auto"
         document.getElementById("ul-" + e.target.id.split('-')[2]).style.display = "none"
 
     }))
@@ -51,86 +51,80 @@ async function dropdown(nom) {
     document.getElementById('in-' + nom).addEventListener("input", (e) => {
         let filterList = document.getElementById(nom)
         const inputValue = e.target.value
-        if(inputValue.length > 1){
+        if (inputValue.length > 1) {
 
             let tagsResults = tags[nom].filter(el => el.includes(inputValue.toLowerCase()))
 
             afficheList(nom, tagsResults)
             ouvreListe(filterList)
-        } else if(true) { //TODO : verifier backspace // DONE 
+        } else if (true) { //TODO : verifier backspace // DONE 
             afficheList(nom, tags[nom])
-        } 
+        }
 
     })
     afficheList(nom, tags[nom]);
 }
-    
 
-async function afficheList(nom, eleAffiche){
+
+async function afficheList(nom, eleAffiche) {
     let filterList = document.getElementById("ul-" + nom)
     filterList.innerHTML = "" //vide la liste
 
-            //sort les listes deroulantes
-        eleAffiche.forEach((e) => {
-            
-            const li = document.createElement('li')
-            li.classList.add(nom + '_open')
-            li.classList.add("col-md-3")
-            li.textContent = e
-               
-            filterList.appendChild(li)  
+    //sort les listes deroulantes
+    eleAffiche.forEach((e) => {
+
+        const li = document.createElement('li')
+        li.classList.add(nom + '_open')
+        li.classList.add("col-md-3")
+        li.textContent = e
+
+        filterList.appendChild(li)
 
 
-            //rapporte le listener aux tags
-            li.addEventListener("click", (el) => {
-                
-                let nameTarget = el.target.textContent;
-                let div = document.createElement("div")
-                div.classList.add('tags')
-                div.classList.add(nom + 'Tags')
-                div.innerText = nameTarget
-                let i = document.createElement("i")
-                i.classList.add("fa-solid")
-                i.classList.add("fa-xmark")
-                i.addEventListener("click", (clic) =>{
-                    div.remove();
-                    //todo : appeler les fonctions de recherche par tags et dans la barre
-                    
-                })
-                div.appendChild(i)
-                tagsSection.appendChild(div)
-                RechercheParTags();
-                
+        //rapporte le listener aux tags
+        li.addEventListener("click", (el) => {
+
+            let nameTarget = el.target.textContent;
+            let div = document.createElement("div")
+            div.classList.add('tags')
+            div.classList.add(nom + 'Tags')
+            div.innerText = nameTarget
+            let i = document.createElement("i")
+            i.classList.add("fa-solid")
+            i.classList.add("fa-xmark")
+            i.addEventListener("click", (clic) => {
+                div.remove();
+                //todo : appeler les fonctions de recherche par tags et dans la barre
+
             })
-            
-        }) //fin du Foreach
+            div.appendChild(i)
+            tagsSection.appendChild(div)
+            RechercheParTags();
+
+        })
+
+    }) //fin du Foreach
 }
 
 let tagSearch
 
 async function RechercheParTags() {
     //recuperer les trois listes de tags
-    
-    console.log(getTags);
+    console.log(nom);
+    console.log(getTags[0]);
     let tagsInsider = Array.from(getTags[0])
-    console.log(tagsInsider)
-    console.log(tags['ingredients']);
+    console.log(tags[nom]);
 
     //filtrer la liste des recettes par rapport aux trois listes de tags
     tagsInsider.forEach((tag =>
-    tagSearch = tags.filter(tag => tag.includes(tag.textContent))))
-    
+        tagSearch = tags.filter(tag => tag.includes(tag.textContent))))
+
     console.log(tagSearch);
 
     //bien afficher DisplayRecipes puis Affichelist 
-        displayRecipes(tagSearch)
+    displayRecipes(tagSearch)
 
- }
-    
-
-
-
-
+}
 
 
 //montre les recettes en bloc
@@ -144,41 +138,41 @@ async function displayRecipes(recipesDisplay) {
     tags['ustensils'] = [];
     //console.log(tags['ustensils']);
     tags['appareils'] = [];
-    
-    tags['ingredients'] = []; 
+
+    tags['ingredients'] = [];
 
 
-    
+
     recipesDisplay.forEach((recipe) => {
 
-            //remplissage des tableaux
-            //console.log(recipe.appliance)
-            appliances = appliances.concat(recipe.appliance.toLowerCase())
-            //tags['appareils'] = [...[recipe.appliance]];
-                    
-            ustensils = ustensils.concat(recipe.ustensils.map(e => e.toLowerCase()))
-            
-            recipe.ingredients.forEach(ingredient => {
-                ingredients.push(ingredient.ingredient.toLowerCase())
+        //remplissage des tableaux
+        //console.log(recipe.appliance)
+        appliances = appliances.concat(recipe.appliance.toLowerCase())
+        //tags['appareils'] = [...[recipe.appliance]];
 
-            })
+        ustensils = ustensils.concat(recipe.ustensils.map(e => e.toLowerCase()))
 
-            //factory d'affichages des blocs
+        recipe.ingredients.forEach(ingredient => {
+            ingredients.push(ingredient.ingredient.toLowerCase())
+
+        })
+
+        //factory d'affichages des blocs
         const recipeEngine = recipesFactory(recipe);
         const recipeDOM = recipeEngine.getRecipeDOM();
         resultsSection.appendChild(recipeDOM);
-        
-        
+
+
     });
     tags['ingredients'] = [...new Set(ingredients)];
     tags['appareils'] = [...new Set(appliances)];
     tags['ustensils'] = [...new Set(ustensils)];
 
     tags['ingredients'].sort()
-    
+
     tags['appareils'].sort()
     tags['ustensils'].sort()
-    
+
 };
 
 //Pour chaque ustensile
